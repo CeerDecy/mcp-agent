e14f4b488e4da62f628dfe5176c86f6f7123426b
CeerDecy <ceerdecy@192.168.0.107>
2025-06-12 22:00:20 +0800

    feat: init mcp client

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -1,8 +1,11 @@
-fn main() {
+
+#[tokio::main]
+async fn main() {
 
     let s = mcp_agent::mcp_server::server::Server::new();
     s.start();
 
     let config = mcp_agent::agent::Config::from_file("mcp-config.toml");
-    let agent = mcp_agent::agent::Agent::new_with_config(config);
+    // println!("{:#?}", config);
+    let _agent = mcp_agent::agent::Agent::new_with_config(config).await;
 }
--- mcp-agent/src/agent/agent.rs
+++ mcp-agent/src/agent/agent.rs
@@ -1,5 +1,7 @@
 use super::config::{Config, McpConfig};
+use rmcp::model::{ClientCapabilities, ClientInfo, Implementation, InitializeRequestParam};
 use rmcp::service::RunningService;
+use rmcp::transport::{SseClientTransport, StreamableHttpClientTransport};
 use rmcp::{
     RoleClient,
     service::ServiceExt,
@@ -12,22 +14,22 @@ use std::sync::Arc;
 use tokio::process::Command;
 
 pub struct Agent {
-    mcp_clients: HashMap<String, Arc<RunningService<RoleClient, ()>>>,
-    tools_clients: HashMap<String, Arc<RunningService<RoleClient, ()>>>,
+    mcp_clients: HashMap<String, McpClient>,
+    tools_clients: HashMap<String, McpClient>,
 }
+
+enum McpClient {
+    StdioClient(Arc<RunningService<RoleClient, ()>>),
+    RemoteClient(Arc<RunningService<RoleClient, InitializeRequestParam>>),
+}
+
+const STREAMABLE_TRANSPORT: &str = "streamable";
+const STDIO_TRANSPORT: &str = "stdio";
+const SSE_TRANSPORT: &str = "sse";
 
 impl Agent {
     pub async fn new_with_config(config: Config) -> Self {
-        // config
-        //     .mcp_servers
-        //     .iter()
-        //     .for_each(|(name, config)| match &config.transport {
-        //         None => {
-        //             initialize_stdio_client(name, &config).await.unwrap();
-        //         }
-        //         Some(transport) => {}
-        //     });
-
+        println!("Starting MCP agent");
         let mut agent = Agent {
             mcp_clients: HashMap::new(),
             tools_clients: HashMap::new(),
@@ -37,30 +39,51 @@ impl Agent {
     }
 
     async fn initialize_mcp(&mut self, config: &Config) {
+        println!("Initializing MCP clients...");
         for (name, mcp_config) in &config.mcp_servers {
             match &mcp_config.transport {
-                Some(transport) => {
-                    match transport.as_str() {
-                        "streamable" => {}
-                        "stdio" => {
-                            let client = initialize_stdio_client(name, mcp_config).await.unwrap();
-                            let c = Arc::new(client);
-                            self.mcp_clients.insert(name.to_owned(), c.clone());
-                            self.tools_clients.insert(name.to_owned(), c.clone());
-                        }
-                        "sse" => {}
-                        _ => {
-                            panic!("Unsupported transport type {}", transport);
-                        },
+                Some(transport) => match transport.as_str() {
+                    STREAMABLE_TRANSPORT => {
+                        let client = Arc::new(
+                            initialize_streamable_client(name, mcp_config)
+                                .await
+                                .unwrap(),
+                        );
+                        self.mcp_clients
+                            .insert(name.to_owned(), McpClient::RemoteClient(client.clone()));
+                        self.tools_clients
+                            .insert(name.to_owned(), McpClient::RemoteClient(client.clone()));
                     }
-                }
+                    STDIO_TRANSPORT => {
+                        let client =
+                            Arc::new(initialize_stdio_client(name, mcp_config).await.unwrap());
+                        self.mcp_clients
+                            .insert(name.to_owned(), McpClient::StdioClient(client.clone()));
+                        self.tools_clients
+                            .insert(name.to_owned(), McpClient::StdioClient(client.clone()));
+                    }
+                    SSE_TRANSPORT => {
+                        let client =
+                            Arc::new(initialize_sse_client(name, mcp_config).await.unwrap());
+                        self.mcp_clients
+                            .insert(name.to_owned(), McpClient::RemoteClient(client.clone()));
+                        self.tools_clients
+                            .insert(name.to_owned(), McpClient::RemoteClient(client.clone()));
+                    }
+                    _ => {
+                        panic!("Unsupported transport type {}", transport);
+                    }
+                },
                 None => {
                     let client = initialize_stdio_client(name, mcp_config).await.unwrap();
                     let c = Arc::new(client);
-                    self.mcp_clients.insert(name.to_owned(), c.clone());
-                    self.tools_clients.insert(name.to_owned(), c.clone());
+                    self.mcp_clients
+                        .insert(name.to_owned(), McpClient::StdioClient(c.clone()));
+                    self.tools_clients
+                        .insert(name.to_owned(), McpClient::StdioClient(c.clone()));
                 }
             }
+            println!("Initialized MCP client: {}", name);
         }
     }
 }
@@ -89,3 +112,62 @@ async fn initialize_stdio_client(
     Ok(client)
 }
 
+async fn initialize_streamable_client(
+    name: &str,
+    config: &McpConfig,
+) -> Result<RunningService<RoleClient, InitializeRequestParam>, Box<dyn Error>> {
+    let url = config.url.clone().expect("MCP url is missing");
+    if url == "" {
+        return Err(format!("mcp [{}] url is empty", name).into());
+    }
+
+    let transport = StreamableHttpClientTransport::from_uri(url);
+
+    let client_info = ClientInfo {
+        protocol_version: Default::default(),
+        capabilities: ClientCapabilities::default(),
+        client_info: Implementation {
+            name: "sse client".to_string(),
+            version: "1.0.0".to_string(),
+        },
+    };
+
+    let client = client_info
+        .serve(transport)
+        .await
+        .inspect_err(|err| {
+            println!("mcp [{}] client error: {}", name, err);
+        })
+        .unwrap();
+
+    Ok(client)
+}
+
+async fn initialize_sse_client(
+    name: &str,
+    config: &McpConfig,
+) -> Result<RunningService<RoleClient, InitializeRequestParam>, Box<dyn Error>> {
+    let url = config.url.clone().expect("MCP url is missing");
+    if url == "" {
+        return Err(format!("mcp [{}] url is empty", name).into());
+    }
+    let transport = SseClientTransport::start(url).await.unwrap();
+    let client_info = ClientInfo {
+        protocol_version: Default::default(),
+        capabilities: ClientCapabilities::default(),
+        client_info: Implementation {
+            name: "sse client".to_string(),
+            version: "1.0.0".to_string(),
+        },
+    };
+
+    let client = client_info
+        .serve(transport)
+        .await
+        .inspect_err(|err| {
+            println!("mcp [{}] client error: {}", name, err);
+        })
+        .unwrap();
+
+    Ok(client)
+}
